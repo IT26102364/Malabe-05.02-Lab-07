2e126320ba080abfea72f4ed308d3c3a2e18a881
IT26102364 <IT26102364@my.sliit.lk>
2026-09-14 00:08:03 +0530

    IT26102364

---
+++ IT26102364Lab7Q2A.java
@@ -0,0 +1,8 @@
+public class IT26102364Lab7Q2A {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 20; i++) {
+            System.out.println("$$$$ ");
+        }
+    }
+}
